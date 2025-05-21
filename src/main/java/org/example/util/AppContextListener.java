35bba5d8dd72976b707c4db183f4350d853355e0
It24100007 <it24100007@my.sliit.lk>
2025-05-21 16:45:34 +0530

    Remove AppContextListener.java

--- src/main/java/org/example/util/AppContextListener.java
+++ src/main/java/org/example/util/AppContextListener.java
@@ -1,16 +1,1 @@
-//initializes the system by creating a default drivers file when the web application starts.
-package org.example.util;
 
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
-
-public class AppContextListener implements ServletContextListener {
-    @Override
-    public void contextInitialized(ServletContextEvent sce) {
-        DriverFileUtil.initializeDefaultDrivers();
-    }
-
-    @Override
-    public void contextDestroyed(ServletContextEvent sce) {
-    }
-}
