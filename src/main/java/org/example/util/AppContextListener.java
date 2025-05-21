23d0f0f118f0a6464518131659cf1ca0ff1cc86a
It24100007 <it24100007@my.sliit.lk>
2025-05-21 15:11:36 +0530

    "Updated AppContextListener"

--- src/main/java/org/example/util/AppContextListener.java
+++ src/main/java/org/example/util/AppContextListener.java
@@ -1,13 +1,13 @@
-//Purpose: Its purpose is to allow your code to "listen" for and react to changes in the web application's lifecycle (startup and shutdown).
+//initializes the system by creating a default drivers file when the web application starts.
 package org.example.util;
 
-import javax.servlet.ServletContextEvent;//s like a memo that says "The building is open for business!" or "The building is closing down for the day!" It's the message about an event
-import javax.servlet.ServletContextListener;//s like the "Building Manager." This manager is responsible for hearing those memos (ServletContextEvents) and performing specific actions based on them. When the "building open" memo arrives, the manager might turn on the lights, unlock the doors, and get everything ready. When the "building closing" memo arrives, the manager might turn off the lights and lock up.
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
 
 public class AppContextListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        DriverFileUtil.initializeDefaultDrivers();//This is the specific instruction for the Shop Manager: "When the shop opens, make sure to turn on the coffee machines and fill the espresso hoppers with beans."
+        DriverFileUtil.initializeDefaultDrivers();
     }
 
     @Override
